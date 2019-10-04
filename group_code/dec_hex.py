84ebb818f55c5a535e19a0479983f4d7f363c614
cybercrypter2810 <megarockystarman@gmail.com>
2019-10-04 13:54:21 -0700

    AA

---
+++ group_code/dec_hex.py
@@ -0,0 +1,17 @@
+#dec_hex.py gbpm kenny_help_thx_bud.
+
+def hexcon(num):
+	key = "0123456789abcdef" # hex key
+	h = ""
+	h16 = int(num/16)
+	h1 = num % 16
+	h = key[h16]+ key[h1]
+	return h
+
+
+def main():
+	hs = "";
+	for i in range(0,256):
+		hs = hexcon(i)
+		print(i,hs)
+main()
