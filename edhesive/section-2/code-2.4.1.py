f7a2d5ce77b117aa93f2b882b69321bdb1c4cc35
cybercrypter2810 <megarockystarman@gmail.com>
2019-10-17 22:04:37 -0700

    AA

---
+++ edhesive/section-2/code-2.4.1.py
@@ -0,0 +1,7 @@
+# Section 2.4.1.py
+
+import math
+s = float(input("Enter a Number to be squarerooted: "))
+x = math.sqrt(math.fabs(s))
+
+print(x)
---
+++ edhesive/section-2/code-2.4.2.py
@@ -0,0 +1,5 @@
+#Section 2.4.2.py
+
+import math
+
+x = float(input("Enter a Number: "))
