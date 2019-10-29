6d1e684b5f241107599d70b94828b6521f526673
cybercrypter2810 <megarockystarman@gmail.com>
2019-10-28 22:02:39 -0700

    AA

---
+++ edhesive/section-2/code-2.8.1.py
@@ -0,0 +1,9 @@
+#Section 2.8.1.py
+
+first = int(input("Enter the First Number: "))
+second = int(input("Enter the Second Number: "))
+third = int(input("Enter the Third Number: "))
+
+maxvalue = max(first,second,third)
+
+print("Largest: " + str(maxvalue))
