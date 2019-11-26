0e5b443955da6a2c90626760aee1ea0782354806
cybercrypter2810 <megarockystarman@gmail.com>
2019-11-25 21:58:25 -0800

    AA

---
+++ edhesive/section-3/code-3.6.1.py
@@ -0,0 +1,25 @@
+# Section 3.6.1.py
+
+i = int(input("Enter a number: "))
+n = i
+print("Largest: " + str(n))
+
+i = int(input("Enter a number: "))
+n = max(i,n)
+print("Largest: " + str(n))
+
+i = int(input("Enter a number: "))
+n = max(i,n)
+print("Largest: " + str(n))
+
+i = int(input("Enter a number: "))
+n = max(i,n)
+print("Largest: " + str(n))
+
+i = int(input("Enter a number: "))
+n = max(i,n)
+print("Largest: " + str(n))
+
+i = int(input("Enter a number: "))
+n = max(i,n)
+print("Largest: " + str(n))
