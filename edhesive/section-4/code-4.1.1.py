56eaf16d0baefeecb3d59c298e8bb1dbd9e0a098
cybercrypter2810 <megarockystarman@gmail.com>
2019-12-20 14:04:56 -0800

    AA

---
+++ edhesive/section-4/code-4.1.1.py
@@ -0,0 +1,10 @@
+# Section 4.1.1.py
+
+name = input("Please Enter a Name. (Type 'Nope' to end.): ")
+
+while (name != "Nope"):
+	print("Nice to meet you " + name)
+	name = input("Please Enter a Name. (Type 'Nope' to end.): ")
+
+print("\n")
+print("Good bye")
